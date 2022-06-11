93c54cae32969455bebde98b8a3fcf15ef0807cb
muthu92 <vennimuthu1992@gmail.com>
2022-06-11 22:06:36 +0530

    Invoice and order report changes updated.

--- packages/Webkul/Admin/src/Exports/OrderDataGridExport.php
+++ packages/Webkul/Admin/src/Exports/OrderDataGridExport.php
@@ -6,6 +6,8 @@ use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Webkul\Product\Models\ProductAttributeValue;
+use Webkul\Product\Models\Product;
+use Illuminate\Support\Facades\DB;
 
 class OrderDataGridExport implements FromView, ShouldAutoSize
 {
@@ -34,7 +36,6 @@ class OrderDataGridExport implements FromView, ShouldAutoSize
     public function view(): View
     {
         $columns = [];
-		
         foreach($this->gridData as $key => $gridData) {
             $columns = array_keys((array) $gridData);
 			
@@ -47,8 +48,6 @@ class OrderDataGridExport implements FromView, ShouldAutoSize
 			if($OrderId != $gridData->ID){
 				$exportData[$key]["ID"]= $gridData->ID;
 				$exportData[$key]["DATE"]= $gridData->DATE;
-				$exportData[$key]["STATUS"]= $gridData->STATUS;
-				$exportData[$key]["CHANNEL_NAME"]= $gridData->CHANNEL_NAME;
 				$exportData[$key]["CUSTOMER_NAME"]= $gridData->CUSTOMER_NAME;
 				$exportData[$key]["PHONE_NO"]= $gridData->PHONE_NO;
  				$exportData[$key]["EMAIL"]= $gridData->EMAIL;
@@ -62,13 +61,13 @@ class OrderDataGridExport implements FromView, ShouldAutoSize
 				$exportData[$key]["SHIPPING_CHARGES"]= $gridData->SHIPPING_CHARGES;
 				$exportData[$key]["CARRIER_TITLE"]= $gridData->CARRIER_TITLE;
 				$exportData[$key]["TRACKING_NUMBER"]=  (string) $gridData->TRACKING_NUMBER;
+				$exportData[$key]["CHANNEL_NAME"]= $gridData->CHANNEL_NAME;
+				$exportData[$key]["STATUS"]= $gridData->STATUS;
 				//$exportData[$key]["BILLING_TO"]= $gridData->BILLING_TO;
 				//$exportData[$key]["SHIPPING_TO"]= $gridData->SHIPPING_TO;
 			}else{
 				$exportData[$key]["ID"]= " ";
 				$exportData[$key]["DATE"]= " ";
-				$exportData[$key]["STATUS"]= " ";
-				$exportData[$key]["CHANNEL_NAME"]= " ";
 				$exportData[$key]["CUSTOMER_NAME"]= " ";
 				$exportData[$key]["PHONE_NO"]= " ";
 				$exportData[$key]["EMAIL"]= " ";
@@ -82,12 +81,19 @@ class OrderDataGridExport implements FromView, ShouldAutoSize
 				$exportData[$key]["SHIPPING_CHARGES"]= " ";
 				$exportData[$key]["CARRIER_TITLE"]= " ";
 				$exportData[$key]["TRACKING_NUMBER"]= " ";
+				$exportData[$key]["CHANNEL_NAME"]= " ";
+				$exportData[$key]["STATUS"]= " ";
 				//$exportData[$key]["BILLING_TO"]= " ";
 				//$exportData[$key]["SHIPPING_TO"]= " ";
 			}
+			$Category=DB::table('product_categories')->leftJoin('category_translations', function ($leftJoin) {
+                $leftJoin->on('category_translations.category_id', '=', 'product_categories.category_id');
+            })->where('product_categories.product_id', $gridData->PRODUCT_ID)->first();
+            $exportData[$key]["CATEGORY"]= $Category->name;
             $exportData[$key]["PRODUCT_NAME"]= $gridData->PRODUCT_NAME;
 			$Barcode=ProductAttributeValue::where("product_id",$gridData->PRODUCT_ID)->where("attribute_id",31)->first();
-			if(isset($Barcode->text_value))
+			
+ 			if(isset($Barcode->text_value))
 				$exportData[$key]["BARCODE"]= $Barcode->text_value;
 			else
 				$exportData[$key]["BARCODE"]= "";
@@ -99,7 +105,9 @@ class OrderDataGridExport implements FromView, ShouldAutoSize
             //$exportData[$key]["HSNCODE"]= $gridData->HSNCODE;
             $exportData[$key]["QTY"]= $gridData->QTY;
             $exportData[$key]["ORDER_AMOUNT"]= $gridData->ORDER_AMOUNT;
-            $exportData[$key]["Total"]= $gridData->Total;
+            $exportData[$key]["TAX_AMOUNT"]= $gridData->TAX_AMOUNT;
+            $exportData[$key]["DISCOUNT"]= $gridData->DISCOUNT;
+            $exportData[$key]["TOTAL"]= $gridData->TOTAL;
 			
 			if($OrderId != $gridData->ID)
 				$OrderId=$gridData->ID;
